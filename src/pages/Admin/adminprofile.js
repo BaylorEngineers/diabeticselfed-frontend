775d4a45ba3c7fe12b4ab5d02794e7fbb6a7f549
tsmanjusha5 <tellasrimanjusha@gmail.com>
2023-11-01 20:33:54 -0500

    - code in login to save access token and user id - code to auto-populate name and email in admin profile

--- src/pages/Admin/adminprofile.js
+++ src/pages/Admin/adminprofile.js
@@ -1,17 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
+import { jwtDecode } from 'jwt-decode';
 
-const AdminProfile = ({ name, onChangeName, onSave, onChangePassword }) => {
+
+const AdminProfile = ({onSave, onChangePassword }) => {
   const [showChangePassword, setShowChangePassword] = useState(false);
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [userData, setUserData] = useState('');
+  const [error, setError] = useState('');
+  const [id, setId] = useState('');
+
+//   const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBtYWlsLmNvbSIsImlhdCI6MTY5ODg3NzMzNCwiZXhwIjoxNjk4OTYzNzM0fQ.kQTkRDTFMK2vNA75XCFHIj87R8a0MNbW05Obsqb3OEc';
+
+   useEffect(() => {
+     const userId = Number(localStorage.getItem('userId'));
+     console.log(userId);
+
+     const accessToken = String(localStorage.getItem('accessToken'));
+     console.log(accessToken);
+
+     const fetchUserData = async () => {
+       try {
+         const response = await fetch('http://localhost:8080/api/v1/users/get-user-data?id=3', {
+           mode: 'cors',
+           method: 'GET',
+           headers: {
+             'Content-Type': 'application/json',
+             'Authorization': `Bearer ${accessToken}`,
+           },
+         });
+
+         if (!response.ok) {
+           throw new Error(`Network response was not ok (${response.status}: ${response.statusText})`);
+         }
+
+         const userData = await response.json();
+         console.log(userData);
+         setUserData(userData);
+         console.log('Fetched userData:', userData);
+       } catch (error) {
+         setError(error.message);
+       }
+     };
+
+     fetchUserData();
+   }, []);
 
   const handleSave = () => {
     // Logic to save the profile changes
-    onSave(name);
   };
 
   const handleChangePassword = () => {
@@ -40,9 +80,13 @@ const AdminProfile = ({ name, onChangeName, onSave, onChangePassword }) => {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
       <div>
       <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => onChangeName(e.target.value)} />
+       Name: {userData.name}
       </label>
+      </div>
+      <div>
+       <label>
+             Email: {userData.email}
+            </label>
       </div>
       <div style={{ marginTop: '20px' }}>
       <Button label="Change Password" onClick={handleChangePassword} size="small"/>
