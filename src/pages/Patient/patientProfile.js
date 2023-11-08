d18eb1f78ec7a065b73e91d26a59153ae5493fe7
tsmanjusha5 <tellasrimanjusha@gmail.com>
2023-11-08 12:02:41 -0600

    -updates to patient profile endpoints

--- src/pages/Patient/patientProfile.js
+++ src/pages/Patient/patientProfile.js
@@ -1,30 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from "../../components/Header/Header";
 import CustomModal from '../SurveyModal';
 import "./css/patientProfile.css";
 
 const PatientProfile = () => {
   const [formData, setFormData] = useState({
+    patientId: '',
     name: '',
     dob: '',
     education: '',
+    type: '',
   });
+
   const [question, setQuestion] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [firstLoginOfTheDay, setFirstLoginOfTheDay] = useState(false);
-  
-  const jwtToken = localStorage.getItem('accessToken');
+  const [updatedProfileType, setUpdatedProfileType] = useState('');
+  const [data, setData] = useState('');
+  const [age, setAge] = useState('');
+  const [levelOfEd, setLevelOfEd] = useState('');
+  const [formChanged, setFormChanged] = useState(false);
+
+  const accessToken = String(localStorage.getItem('accessToken'));
+  console.log(accessToken);
+
+  const patientId = localStorage.getItem('patientId');
+  console.log(patientId)
+
+  const calculateAge = useCallback((dobString) => {
+      if (!dobString) {
+        return null;
+      }
+
+      const birthDate = new Date(dobString);
+      const today = new Date();
+
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      return age;
+    }, []);
 
   useEffect(() => {
-    const fetchSurvey = async () => {
+    const fetchPatientData = async () => {
       setLoading(true);
       try {
-        const patientId = localStorage.getItem('patientId'); // Assuming you need to use patientId
-        const response = await fetch(`http://localhost:8080/api/v1/question/get/1`, {
+        const response = await fetch('http://localhost:8080/api/v1/patient-profile/detail/' + patientId, {
           headers: {
-            Authorization: `Bearer ${jwtToken}`,
+            Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
           },
         });
@@ -34,21 +63,77 @@ const PatientProfile = () => {
         }
 
         const data = await response.json();
-        setQuestion(data.description);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+        const age = calculateAge(data.dob);
+        setAge(age);
+        const levelOfEd = data.levelOfEd.toLowerCase();
+        setLevelOfEd(levelOfEd);
+        console.log("age and levelOfEd:", age);
+        console.log(levelOfEd);
+        const updatedProfileType = calculateProfileType(age, levelOfEd);
+        setUpdatedProfileType(updatedProfileType);
+        console.log("fetched patient data-",data);
+        console.log("profile type:", updatedProfileType);
+        setFormData((prevData) => ({
+                ...prevData,
+                patientId:data.id,
+                name: data.name,
+                dob: data.dob.split('T')[0],
+                education: data.levelOfEd,
+                type: updatedProfileType,
+              }));
+              console.log("Updated formData:", formData);
+              console.log(formChanged);
+            } catch (error) {
+              setError(error.message);
+            } finally {
+              setLoading(false);
+            }
+          };
 
-    fetchSurvey();
-  }, [jwtToken]);
+          fetchPatientData();
+        }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
+
+
+  useEffect(() => {
+//  console.log("value of form before", formChanged);
+    if (formData.education) {
+      const newAge = calculateAge(data.dob);
+      const newLevelOfEd = formData.education.toLowerCase();
+
+      const updatedProfileType = calculateProfileType(newAge, newLevelOfEd);
+
+      setUpdatedProfileType(updatedProfileType);
+      setFormData((prevData) => ({
+        ...prevData,
+        type: updatedProfileType,
+      }));
+
+      setFormChanged(true);
+    }
+
+  }, [formData.education, data.dob]);
+
+  console.log("value of form changed", formChanged);
+
+    const calculateProfileType = (age, education) => {
+      if ((age < 50 && ['highschool', 'undergraduate', 'graduate'].includes(education))) {
+        return 'TYPE1';
+      } else if (age >= 50 && education === 'other') {
+        return 'TYPE2';
+      } else if (education === 'other') {
+        return 'TYPE3';
+      }
+      return '';
+    };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -58,7 +143,7 @@ const PatientProfile = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${jwtToken}`,
+          Authorization: `Bearer ${accessToken}`,
         },
         body: JSON.stringify(formData),
       });
@@ -120,17 +205,23 @@ const PatientProfile = () => {
           </div>
           <div>
             <label htmlFor="education">Education:</label>
-            <input
-              type="text"
-              id="education"
-              name="education"
-              value={formData.education}
-              onChange={handleChange}
-              required
-            />
-          </div>
+            <select
+                id="education"
+                name="education"
+                value= {formData.education}
+                onChange={handleChange}
+                required
+              >
+              <option value=''>Select education level</option>
+                <option value="HIGHSCHOOL">High School</option>
+                <option value="UNDERGRADUATE">Undergraduate</option>
+                <option value="GRADUATE">Graduate</option>
+                <option value="OTHER">Other</option>
+              </select>
+            </div>
+            <p>education: {formData.education}</p>
 
-          <button className="submit-button" type="submit">Submit</button>
+          <button className="submit-button" type="submit" disabled={!formChanged}>Submit</button>
         </form>
       </div>
       {loading && <p>Loading...</p>}
