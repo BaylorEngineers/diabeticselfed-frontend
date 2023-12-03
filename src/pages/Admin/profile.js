54da34668ab3e541ed188d5e720e704a02465b8f
tsmanjusha5 <tellasrimanjusha@gmail.com>
2023-12-03 06:54:13 -0600

    -updated profile and accountmanager styling

--- src/pages/Admin/profile.js
+++ src/pages/Admin/profile.js
@@ -1,69 +1,77 @@
 import React, { useState, useEffect } from 'react';
 import Header from "../../components/Header/Header";
-
+import "../Patient/css/patientProfile.css";
 
 const Profile = () => {
   const role = localStorage.getItem('role');
 
-  const [userData, setUserData] = useState('');
+  const [userData, setUserData] = useState({});
   const [error, setError] = useState('');
-  const [id, setId] = useState('');
-  const [accessToken, setAccessToken] = useState('');
 
+  useEffect(() => {
+    const userId = Number(localStorage.getItem('userId'));
+    const accessToken = String(localStorage.getItem('accessToken'));
 
-   useEffect(() => {
-     const userId = Number(localStorage.getItem('userId'));
-     console.log(userId);
-     setId(userId);
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/v1/users/get-user-data?id=${userId}`, {
+          mode: 'cors',
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+          },
+        });
 
-     const accessToken = String(localStorage.getItem('accessToken'));
-     console.log(accessToken);
-     setAccessToken(accessToken);
+        if (!response.ok) {
+          throw new Error(`Network response was not ok (${response.status}: ${response.statusText})`);
+        }
 
-     const fetchUserData = async () => {
-       try {
-         const response = await fetch(`http://localhost:8080/api/v1/users/get-user-data?id=${userId}`, {
-           mode: 'cors',
-           method: 'GET',
-           headers: {
-             'Content-Type': 'application/json',
-             'Authorization': `Bearer ${accessToken}`,
-           },
-         });
+        const userData = await response.json();
+        console.log('Fetched userData:', userData);
+        setUserData(userData);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
 
-         if (!response.ok) {
-           throw new Error(`Network response was not ok (${response.status}: ${response.statusText})`);
-         }
-
-         const userData = await response.json();
-         console.log(userData);
-         setUserData(userData);
-         console.log('Fetched userData:', userData);
-       } catch (error) {
-         setError(error.message);
-       }
-     };
-
-     fetchUserData();
-   }, []);
+    fetchUserData();
+  }, []);
 
   return (
-      <>
-        <Header role={role} />
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+    <>
+      <Header role={role} />
+      <div className="patient-profile-container">
+        <h1>My Profile</h1>
+        {error && <p className="error-message">{error}</p>}
+        <form className="patient-profile-form">
           <div>
-            <label>
-              Name: {userData.firstName} {userData.lastName}
-            </label>
+            <label htmlFor="name">First Name:</label>
+            <input
+              type="text"
+              id="firstname"
+              name="firstname"
+              value={userData.firstName || ''}
+              readOnly
+            />
           </div>
           <div>
-            <label>
-              Email: {userData.email}
-            </label>
+            <label htmlFor="name">Last Name:</label>
+            <input
+              type="text"
+              id="lastname"
+              name="lastname"
+              value={userData.lastName || ''}
+              readOnly
+            />
           </div>
-        </div>
-      </>
-    );
-  };
+          <button className="submit-button" type="submit" disabled>
+            Save
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
